e99073daba074882003e17ad2fce93534e88e72c
seven <728443778@qq.com>
2017-11-08 17:49:07 +0800

    补货httprequest中的异常

--- src/HttpRequest.php
+++ src/HttpRequest.php
@@ -8,22 +8,30 @@ class HttpRequest
 
     public static function get($url)
     {
-        $url = urlencode($url);
-        return file_get_contents($url);
+        try {
+            $url = urlencode($url);
+            return file_get_contents($url);
+        } catch (\Exception $exception) {
+            return $exception->getMessage() . ':' . $exception->getTraceAsString();
+        }
     }
 
     public static function post($url, $data)
     {
-        $data = http_build_query($data);
-        $http = [
-            'http' => [
-                'method' => 'POST',
-                'header' => "Content-type:application/x-www-form-urlencoded\r\n" . 'Content-Length:' . strlen($data) . "\r\n",
-                'content' => $data
-            ],
-        ];
-        $context = stream_context_create($http);
-        $url = urlencode($url);
-        return file_get_contents($url, false, $context);
+        try {
+            $data = http_build_query($data);
+            $http = [
+                'http' => [
+                    'method' => 'POST',
+                    'header' => "Content-type:application/x-www-form-urlencoded\r\n" . 'Content-Length:' . strlen($data) . "\r\n",
+                    'content' => $data
+                ],
+            ];
+            $context = stream_context_create($http);
+            $url = urlencode($url);
+            return file_get_contents($url, false, $context);
+        } catch (\Exception $exception) {
+            return $exception->getMessage() . ':' . $exception->getTraceAsString();
+        }
     }
 }
