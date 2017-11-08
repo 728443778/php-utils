795ab651a24253263393673a4ad831006e5fb003
seven <728443778@qq.com>
2017-11-08 15:13:11 +0800

    添加httprequest

---
+++ src/HttpRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace utils;
+
+class HttpRequest
+{
+
+
+    public static function get($url)
+    {
+        return file_get_contents($url);
+    }
+
+    public static function post($url, $data)
+    {
+        $data = http_build_query($data);
+        $http = [
+            'http' => [
+                'method' => 'POST',
+                'header' => "Content-type:application/x-www-form-urlencoded\r\n" . 'Content-Length:' . strlen($data) . "\r\n",
+                'content' => $data
+            ],
+        ];
+        $context = stream_context_create($http);
+        return file_get_contents($url, false, $context);
+    }
+}
