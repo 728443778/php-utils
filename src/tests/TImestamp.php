535223b4578e8605aa2720f6b77eca34bf15e77b
seven <728443778@qq.com>
2018-05-08 09:42:11 +0800

    add timestamp test

---
+++ src/tests/TImestamp.php
@@ -0,0 +1,19 @@
+<?php
+
+date_default_timezone_set('Africa/Abidjan');
+echo 'Africa/Abidjan time zone,the unix time is:',time(),PHP_EOL;
+echo 'date:',date('Y-m-d H:i:s'),PHP_EOL;
+
+date_default_timezone_set('America/Cayman');
+echo 'America/Cayman time zone,the unix time is:',time(),PHP_EOL;
+echo 'date:',date('Y-m-d H:i:s'),PHP_EOL;
+
+date_default_timezone_set('Arctic/Longyearbyen');
+echo 'Arctic/Longyearbyen time zone, the unix time is:',time(),PHP_EOL;
+echo 'date:',date('Y-m-d H:i:s'),PHP_EOL;
+
+
+date_default_timezone_set('Asia/Shanghai');
+echo 'Asia/Shanghai time zone, the unix time is :',time(),PHP_EOL;
+echo 'date:',date('Y-m-d H:i:s'),PHP_EOL;
+
